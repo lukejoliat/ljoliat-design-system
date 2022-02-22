687ada386c674e2416edacf1bb5ed17ad6088ea4
Luke <luke@pop-os.localdomain>
2022-02-21 22:16:35 -0500

    Version 4

--- packages/index.js
+++ packages/index.js
@@ -1,9 +1,11 @@
-import Button from "./button/button";
-import Input from "./input/input";
-import Text from "./text/text";
-import { DesignSystemTheme } from "./theme/themeProvider";
+import Button from './button/button';
+import Input from './input/input';
+import Text from './text/text';
+import { DesignSystemTheme } from './theme/themeProvider';
+import Card from './card/card';
 
 export { Input };
 export { Button };
-export { DesignSystemTheme }
+export { DesignSystemTheme };
 export { Text };
+export { Card };
